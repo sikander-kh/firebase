2f0bc126dfcecdf9aa86ee433cd13482ab45fda5
sikander <sikanderbhutto1@gmail.com>
2017-05-16 13:01:16 +0530

    context changed

--- app/src/main/java/com/test/firebase/MyFirebaseInstanceIDService.java
+++ app/src/main/java/com/test/firebase/MyFirebaseInstanceIDService.java
@@ -33,7 +33,7 @@ public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
     private void sendRegistrationToServer(String token)
     {
 
-        new DevicePreferences().addKey(context, Constants.FIREBASE_TOKEN, token);
+        new DevicePreferences().addKey(this, Constants.FIREBASE_TOKEN, token);
 
         //You can implement this method to store the token on your server
         //Not required for current project
